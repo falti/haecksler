2dbb39753ac7574e0d9ede60af100e2677769e2b
Frank Falkenberg <faltibrain@gmail.com>
2015-01-05 16:40:53 +0100

    :lipstick:

--- lib/haecksler/row.rb
+++ lib/haecksler/row.rb
@@ -20,24 +20,30 @@ module Haecksler
     end
 
     def parse(input)
-
-      total_number_of_characters = columns.map(&:size).reduce(&:+)
       input = input.slice(0, total_number_of_characters)
+      @columns = parsed_columns(slices(input))
+      self
+    end
 
-      indizes = columns.map(&:size).reduce([0]) do |memo,item|
+    def total_number_of_characters
+      columns.map(&:size).reduce(&:+)
+    end
+
+    def parsed_columns(slices)
+      slices.map {|slice| slice.map{|i| i[0]}.join("")}.zip(columns).map do |text, column|
+        column.parse(text)
+      end
+    end
+
+    def indizes
+      columns.map(&:size).reduce([0]) do |memo,item|
         memo << (memo.last.to_i + item)
         memo
       end
+    end
 
-      slices = input.split(//).each_with_index.slice_before { | element | indizes.include? element[1] }
-
-      parsed_columns = slices.map {|slice| slice.map{|i| i[0]}.join("")}.zip(columns).map do |text, column|
-        column.parse(text)
-      end
-
-      @columns = parsed_columns
-
-      self
+    def slices(input)
+      input.split(//).each_with_index.slice_before { | element | indizes.include? element[1] }
     end
 
     def header?
